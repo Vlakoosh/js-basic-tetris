c1836664e4f579b2098a2672b5d8b7e6a580a491
Szymon Walkusz <szymon.walkusz@student.kdg.be>
2024-05-27 14:11:46 +0200

    successfully initialized the playing field

--- tetris.js
+++ tetris.js
@@ -1,5 +1,34 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+// const canvas = document.getElementById("canvas");
+// const ctx = canvas.getContext("2d");
+
+const FIELD_WIDTH = 12;
+const FIELD_HEIGHT = 18;
+
+let field = new Array(FIELD_WIDTH * FIELD_HEIGHT);
+
+
+(function initializeField(){
+    for (let i = 0; i < field.length; i++) {
+        let fy = Math.floor(i/FIELD_WIDTH);
+        let fx = i % FIELD_WIDTH;
+        if(fy === FIELD_HEIGHT - 1 || fx === 0 || fx === FIELD_WIDTH-1){
+            field[i] = "#"
+        }
+        else field[i] = " ";
+    }
+})();
+console.log(field);
+
+function printField() {
+    for (let y = 0; y < FIELD_HEIGHT; y++){
+        for (let i = 0; i < FIELD_WIDTH; i++) {
+            process.stdout.write(field[y*FIELD_WIDTH + i]);
+        }
+        console.log();
+
+    }
+}
+printField();
 
 let tetrominos = ["","","","","","",""];
 
@@ -37,3 +66,13 @@ tetrominos[6] += "....";
 tetrominos[6] += ".XX.";
 tetrominos[6] += ".X..";
 tetrominos[6] += ".X..";
+
+function rotate(px, py, rotation) {
+    switch (rotation % 4){
+        case 0: return py * 4 + px;         //0 degrees
+        case 1: return 12 + py - (px * 4);  //90 degrees
+        case 2: return 15 - (py * 4) - px;  //180 degrees
+        case 3: return 3 - py + (px * 4);   //270 degrees
+    }
+    return 0;
+}
